2f0ae74cfd67eb80b04564ce9904be7472385aff
zhoupengwei <zhoupengwei@digilink-tech.com>
2019-11-14 12:31:58 +0800

    update

--- src/main/java/com/pengwei/zhou/app/SpringBootWebApplication.java
+++ src/main/java/com/pengwei/zhou/app/SpringBootWebApplication.java
@@ -2,11 +2,10 @@ package com.pengwei.zhou.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
-    public static void main(String[] args) throws Exception {
+public class SpringBootWebApplication {
+    public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
 }
---
+++ src/main/java/com/pengwei/zhou/app/service/Impl/TestServiceImpl.java
@@ -0,0 +1,30 @@
+package com.pengwei.zhou.app.service.Impl;
+
+import com.pengwei.zhou.app.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestServiceImpl implements TestService {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Override
+  public void create(String name, Integer age) {
+    jdbcTemplate.update("insert into USER_1(NAME, AGE) values(?, ?)", name, age);
+  }
+  @Override
+  public void deleteByName(String name) {
+    jdbcTemplate.update("delete from USER_1 where NAME = ?", name);
+  }
+  @Override
+  public Integer getAllUsers() {
+    return jdbcTemplate.queryForObject("select count(1) from USER_1", Integer.class);
+  }
+  @Override
+  public void deleteAllUsers() {
+    jdbcTemplate.update("delete from USER_1");
+  }
+}
