2f20c128108fd50c00ecfd9394249e585f4c690b
zhoupengwei <zhoupengwei@digilink-tech.com>
2019-11-22 15:24:57 +0800

    add Camunda

--- src/main/java/com/pengwei/zhou/app/SpringBootWebApplication.java
+++ src/main/java/com/pengwei/zhou/app/SpringBootWebApplication.java
@@ -1,11 +1,26 @@
 package com.pengwei.zhou.app;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class SpringBootWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    public void processPostDeploy(PostDeployEvent postDeployEvent) {
+        runtimeService.startProcessInstanceByKey("Process_test_1");
+    }
+
 }
